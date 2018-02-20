8527e2af5440f5a5ccb39e15362137940f8971cf
nikhilravigarg <36602151+nikhilravigarg@users.noreply.github.com>
2018-02-20 20:52:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,13 @@ app.get('/ui/article-one.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
+app.get('/ui/article-two.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
 
+app.get('/ui/article-three.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
